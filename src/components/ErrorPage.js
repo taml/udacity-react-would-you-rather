77e92902c28d35d488447b8eefc6fef603e27700
Tam Love <dark_angel4eva@email.com>
2021-06-03 17:39:04 +0100

    Styled Error page.

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import Nav from './Nav'
+import { EmojiSadIcon } from '@heroicons/react/outline'
 
 function ErrorPage(props) {
     return(
-        <div>
+        <div className="bg-gray-50">
             <Nav />
-            <h3>Error 404</h3>
-            <p>{props.errorMessage}</p>
+            <div className="container -mt-14 mx-auto px-4">
+                <div className="flex h-screen justify-center items-center">
+                    <div className="w-2/5 text-center bg-gray-700 rounded-xl">
+                        <EmojiSadIcon className="w-24 text-indigo-500 mx-auto pt-24" />
+                        <h3 className="font-extrabold text-3xl text-white pt-10 pb-10">Error 404</h3>
+                        <p className="text-white px-8 pb-24">{props.errorMessage}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
